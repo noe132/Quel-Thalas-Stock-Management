35826786217c7b3f7eb90bcc4bedbb0fc3358c58
noe132 <nnnnoe132@gmail.com>
2016-11-04 18:28:47 +0800

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var MySQLStore = require('express-mysql-session')(session);
 require('./DateExtension').dateExtension();
 
 const options = {
-	host: 'localhost',
+	host: '127.0.0.1',
 	port: 3306,
 	user: 'root',
 	password: '123456',
